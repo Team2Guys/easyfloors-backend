9189fe492997d75d8ddd6991429a0e9b22191274
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2026-02-05 16:54:34 +0500

    added frontend url in cors allow array

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,7 @@ async function bootstrap() {
       'http://185.151.51.28:3001',
       'http://185.151.51.28:3000',
       'http://localhost:5007',
-      'https://easyfloors-backend.vercel.app',
+      'https://easyfloors-frontend.vercel.app/',
     ],
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-Type'],
